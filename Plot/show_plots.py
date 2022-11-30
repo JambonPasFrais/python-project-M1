83fadeb728e9b9dc11a981fc9765b40e7cac16d7
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2022-11-30 11:06:36 +0100

    Show team plot

--- Plot/show_plots.py
+++ Plot/show_plots.py
@@ -1,5 +1,6 @@
 import os
 import player_plot
+import team_plot
 
 #get the list of all the files in the players_stats directory
 players_list = os.listdir("../Scrapping/players_stats")
@@ -18,5 +19,23 @@ try:
     else:
         player_plot.plot_data(players_list[choice - 1])
         player_plot.player_rank_evo(players_list[choice - 1])
+except:
+    print("Invalid input, try again")
+
+teams_list = os.listdir("../Scrapping/teams_stats")
+teams_list.sort()
+print("Choose the team :")
+#Remove .csv extension and print the name of the player
+for i in range(len(teams_list)):
+    teams_list[i] = teams_list[i].replace('.csv','')
+    print(str(i+1)+". "+teams_list[i])
+
+
+try:
+    choice = int(input("Enter your choice: "))
+    if choice > len(teams_list) or choice < 1:
+        print("Invalid choice, try again")
+    else:
+        team_plot.plot_data(teams_list[choice - 1])
 except:
     print("Invalid input, try again")
