4404c3d3da94bf4c6c9ac045bed73957ccb419f8
Thayo <90453341+TheoRondoux@users.noreply.github.com>
2022-12-02 13:19:49 +0100

    improvements

--- Plot/show_plots.py
+++ Plot/show_plots.py
@@ -12,22 +12,26 @@ for i in range(len(players_list)):
     players_list[i] = players_list[i].replace('.csv','')
     print(str(i+1)+". "+players_list[i])
 
-
+#MARIE - players_list : liste de tous les joueurs qui ont été scraps
 try:
     choice = int(input("Enter your choice: "))
     if choice > len(players_list) or choice < 1:
         print("Invalid choice, try again")
     else:
-        has_played = player_plot.hasPlayed(players_list[choice - 1])
+        """A UTILISER PAR MARIE :)"""
+        #Choice : var pour le nom du player (Prénom Nom)
+        choice = players_list[choice - 1]
+        #Appeler ces fonctions
+        has_played = player_plot.hasPlayed(choice)
+        #Si le joueur a joué dans au moins 1 saison
         if has_played > 0:
-            player_plot.plot_data(players_list[choice - 1])
+            season = '2022-23'
             print("Generating plots...")
-            player_plot.player_rank_evo(players_list[choice - 1])
-            player_plot.plus_minus(players_list[choice - 1])
-            player_plot.win_lose(players_list[choice - 1])
+            player_plot.plot_all_data(choice, season)
             print("Files generated ! You can find them in the player_plot_images folder.")
         else:
             print("The player has not played yet.")
+        """FIN DE L'UTILISATION PAR MARIE :)"""
 except:
     print("Invalid input, try again")
 
