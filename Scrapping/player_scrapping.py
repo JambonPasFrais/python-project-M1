382d7134c012ac4359ac70e9bbc1cebd9bcc62e2
Jambon <armand.deffrennes@student.junia.com>
2022-12-02 11:16:09 +0100

    Added TeamStats & PlayerStats

--- Scrapping/player_scrapping.py
+++ Scrapping/player_scrapping.py
@@ -8,20 +8,24 @@ def save_players_stats_to_csv(player_stats, player_name):
 def scrap_all_players_data_to_csv(players):
     players = players.get_active_players()  # 582 players in total
 
-    '''for player in players:
+    for player in players:
         player_id = player['id']
         player_name = player['full_name']
 
         try:
             player_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
             player_stats = player_stats.get_data_frames()[0]
+            
+            player_stats['PLAYER_NAME'] = player_name
+
+            save_players_stats_to_csv(player_stats, player_name)
         except:
             print('Error with player: ', player_name)
             continue
 
         print('Player: ', player_name)
-        players_stats[player_name] = player_stats'''
 
+    '''
     for i in range(0, 5):
         player_id = players[i]['id']
         player_name = players[i]['full_name']
@@ -39,5 +43,6 @@ def scrap_all_players_data_to_csv(players):
         except:
             print('Error with player: ', player_name)
             continue
+            '''
 
     print('Scraping and saving of players finished')
