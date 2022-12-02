bedf1a69d11a69bb9cc9f75df738fd8c5ca7392a
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2022-12-02 21:13:41 +0100

    apperance

--- Plot/team_plot.py
+++ Plot/team_plot.py
@@ -1,7 +1,14 @@
-from nba_api.stats.endpoints import playercareerstats
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
 import pandas as pd
 import seaborn as sns
+import plotly.express as px
 import csv
 
 def extract_csv_data(team):
@@ -35,8 +42,8 @@ def plot_histogram(team) :
     fg3_list = []
     season_list = []
     for i in range(len(rows_list)-10, len(rows_list)):
-        fg2_list.append(rows_list[i][18])
-        fg3_list.append(rows_list[i][21])
+        fg2_list.append(int(rows_list[i][18]))
+        fg3_list.append(int(rows_list[i][21]))
         season_list.append(rows_list[i][3])
 
     fg2_dict = {}
@@ -88,14 +95,54 @@ def plot_correlation(team):
     sns.heatmap(df.corr())
     plt.savefig("team_plot_images/correlation " + team + ".png")
 
+# Create a spider plot
+def plot_spider(team):
+    rows_list = extract_csv_data(team)
+    data_list = []
+    for i in range(len(rows_list)-3, len(rows_list)):
+        tmp_list = []
+        tmp_list.append(int(rows_list[i][24]))
+        tmp_list.append(int(rows_list[i][25]))
+        tmp_list.append(int(rows_list[i][29]))
+        tmp_list.append(int(rows_list[i][30]))
+        tmp_list.append(int(rows_list[i][31]))
+        data_list.append(tmp_list)
 
+
+    N = 9
+    theta = radar_factory(N, frame='polygon')
+    spoke_labels = data_list.pop(0)
+    fig, axs = plt.subplots(figsize=(9, 9), nrows=2, ncols=2,
+                            subplot_kw=dict(projection='radar'))
+    fig.subplots_adjust(wspace=0.25, hspace=0.20, top=0.85, bottom=0.05)
+    colors = ['b', 'r', 'g']
+    for ax, (title, case_data) in zip(axs.flat, data):
+        ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
+        ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
+                     horizontalalignment='center', verticalalignment='center')
+        for d, color in zip(case_data, colors):
+            ax.plot(theta, d, color=color)
+            ax.fill(theta, d, facecolor=color, alpha=0.25, label='_nolegend_')
+        ax.set_varlabels(spoke_labels)
+
+        # add legend relative to top-left plot
+    labels = ('Factor 1', 'Factor 2', 'Factor 3', 'Factor 4', 'Factor 5')
+    legend = axs[0, 0].legend(labels, loc=(0.9, .95),
+                              labelspacing=0.1, fontsize='small')
+
+    fig.text(0.5, 0.965, '5-Factor Solution Profiles Across Four Scenarios',
+             horizontalalignment='center', color='black', weight='bold',
+             size='large')
+
+    plt.show()
 
 # Test
 team1 = "Cleveland Cavaliers"
 team2 = "Chicago Bulls"
-plot_evolution(team1)
-plot_evolution(team2)
-plot_histogram(team1)
-plot_histogram(team2)
-plot_correlation(team1)
-plot_correlation(team2)
+#plot_evolution(team1)
+#plot_evolution(team2)
+#plot_histogram(team1)
+#plot_histogram(team2)
+#plot_correlation(team1)
+#plot_correlation(team2)
+plot_spider(team1)
